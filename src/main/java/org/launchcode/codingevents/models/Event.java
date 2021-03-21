afd288bc9f4bb7d32a05aa97131b484f3759da31
AmberBaumann <ambermindsolliving@gmail.com>
2021-03-21 14:47:06 -0500

    model-validation

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,17 +1,23 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
+import java.util.Date;
 import java.util.Objects;
 
 /**
  * Created by Chris Bay
  */
+@Entity
 public class Event {
 
+    @Id
+    @GeneratedValue
     private int id;
-    private static int nextId = 1;
 
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
@@ -24,17 +30,30 @@ public class Event {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
-        this();
+    @NotBlank(message = "Location is required.")
+    @NotNull(message = "Location is required.")
+    private String location;
+
+    @NotNull(message = "Number of attendees is required.")
+    @Min(value = 1, message = "Event requires at least 1 attendee.")
+    private int numberOfAttendees;
+
+    @AssertTrue
+    private boolean registrationRequired;
+
+    private EventType type;
+
+    public Event(String name, String description, String contactEmail, String location, int numberOfAttendees, EventType type, boolean registrationRequired) {
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.location = location;
+        this.numberOfAttendees = numberOfAttendees;
+        this.type = type;
+        this.registrationRequired = registrationRequired;
     }
 
-    public Event() {
-        this.id = nextId;
-        nextId++;
-    }
+    public Event() { }
 
     public String getName() {
         return name;
@@ -58,6 +77,38 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
+
+    public boolean isRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
     }
 
     public int getId() {
